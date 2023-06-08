12f9c767052a0521838db42a562dd40c6e435024
chriiss <christopher.kumla@gmail.com>
2023-06-08 18:32:20 +0200

    change deployment

--- strapi/config/plugin.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ env }) => ({
-    "netlify-deployments": {
-        enabled: true,
-        config: {
-            accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN
-        },
-    },
-});
